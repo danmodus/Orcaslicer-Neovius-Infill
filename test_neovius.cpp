6e780d94c8f5fd93cb18c46926e83a515acc9c24
Daniel Shen <danshen@Dans-MacBook-Air.local>
2025-12-27 23:53:08 -0500

    Initial commit: OrcaSlicer with Neovius Infill support

---
+++ test_neovius.cpp
@@ -0,0 +1,9 @@
+#include "src/libslic3r/Fill/FillNeovius.hpp"
+#include <iostream>
+
+int main()
+{
+    Slic3r::FillNeovius fill;
+    std::cout << "Neovius pattern initialized." << std::endl;
+    return 0;
+}
